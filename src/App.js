d02e1fe893cb1c9ad3aa542fbba2da7b732e972c
Allam Vachaspati <allamvachaspati05@gmail.com>
2021-11-15 20:43:20 +0530

    client side coding

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
